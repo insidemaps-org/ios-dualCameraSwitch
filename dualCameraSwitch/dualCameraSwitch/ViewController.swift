8f9a5b55320e0dc2c2b31d852334833397f17f90
Aleksandar Ristanovic <aleksa.ristanovic@gmail.com>
2019-01-25 17:02:52 +0100

    Initial project commit

---
+++ dualCameraSwitch/dualCameraSwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dualCameraSwitch
+//
+//  Created by Aleksandar Ristanovic on 1/25/19.
+//  Copyright © 2019 Aleksandar Ristanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
